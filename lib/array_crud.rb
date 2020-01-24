4036eb08a3fd756a6e9cb02f70d8eb7e9f1d9a19
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-01-24 12:39:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["Fire","Earth","Wind","Ice","arrays!"]
-  element = 
+  element = array.pop
 end
 
 def remove_element_from_start_of_array(array)
